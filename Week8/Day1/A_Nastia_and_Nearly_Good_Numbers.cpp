cad342d3b10096ecc807549fa8ee4b36fabd888b
Farzana <123249663+cool-programmer101@users.noreply.github.com>
2024-05-11 23:28:41 +0600

    Add files via upload
    added week8 day1 codes

---
+++ Week8/Day1/A_Nastia_and_Nearly_Good_Numbers.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        long long A,B;
+        cin>>A>>B;
+        if(B==1)
+            cout<<"NO\n";
+        else
+        {
+            cout<<"YES\n";
+            cout<<A<<" "<<A*B<<" "<<A*(B+1)<<endl;
+        }
+    }
+}
